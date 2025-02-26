0a7e91482b70da0bce0e09bc97b5cd18d3c922f6
Sergi Estall <sergiestall99@gmail.com>
2025-02-26 10:37:51 +0100

    improved version with rating

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # IB-Feed-to-Mastodon-Bot
 A bot that posts your Infinite Backlog updates to Mastodon
+
+# Local run
+Create a virtual environment and use: 
+pip install -r requirements.txt
--- ib-to-mast-toot.py
+++ ib-to-mast-toot.py
@@ -1,83 +1,106 @@
 import os
 import re
 
-import chromedriver_autoinstaller
+from dotenv import load_dotenv
 from mastodon import Mastodon
-from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-mast_tag = "Eigrest_IB_bot"
+MAST_TAG = "Eigrest_IB_bot"
 
-# create webdriver object
-display = Display(visible=0, size=(800, 800))
-display.start()
+def load_page(uri):
+    driver.get(uri)
+    # Waits page to load.
+    WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "post")))
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-# and if it doesn't exist, download it automatically,
-# then add chromedriver to path
+def filter_game_updates():
+    feed = driver.find_elements("xpath", '//*[contains(@class,"COLLECTION_GAME_COMPLETION") or contains(@class,"COLLECTION_GAME_STATUS")]')
+    return [
+        element
+        for post in feed
+        for element in post.find_elements("xpath", './*[contains(@class, "message-post")]')
+    ]
+            
+def format_game_updates_text():
+    return [
+        # removes x days ago text and adds rating
+        re.sub(r"- \d+.*", "", element.text) + add_game_ratings(element)
+        for element in game_updates_elements
+    ]
 
-chrome_options = webdriver.ChromeOptions()
-options = [
-    "--window-size=1200,1200",
-    "--ignore-certificate-errors"
-]
+def add_game_ratings(element):
+    star_map = {"100%": "🌕", "50%": "🌗", "0%": "🌑"}
 
-for option in options:
-    chrome_options.add_argument(option)
+    return "".join(
+        star_map.get(
+            star.find_element(By.TAG_NAME,"stop").get_attribute("offset"), ""
+        )
+        for star_rating in element.find_elements(By.CLASS_NAME, "star-rating-star")
+        for star in star_rating.find_elements(By.TAG_NAME, "svg")
+    )
 
-driver = webdriver.Chrome(options=chrome_options)
+def get_latest_bot_toot():
+    last_toot = mastodon.account_statuses(mastodon.me().get("id"),False,False,
+                                    True,True,MAST_TAG,
+                                    None,None,None,None)[0].content
+    #normalize toot for later matching
+    last_toot = re.sub(r'\s+', ' ', last_toot.replace('<br />', ' ').replace('<p>', '').replace('</p>', '').strip()).lower()
+    
+    print("last toot:")
+    print(last_toot)
+    return last_toot
 
-driver.get("https://infinitebacklog.net/users/eigrest")
-WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "post")))
-feed = driver.find_elements("xpath", '//*[contains(@class,"COLLECTION_GAME_COMPLETION") or contains(@class,"COLLECTION_GAME_STATUS")]')
-posts = []
-for post in feed:
-    posts.extend(post.find_elements("xpath", './*[contains(@class, "message-post")]'))
-posts_texts = []
-for post in posts:
-    # remove X days ago text
-    clean_post = re.sub(r"- \d+.*", "", post.text)
-    posts_texts.append(clean_post)
+def is_game_update_in_toot(game_update):
+    normalized_game_update = " ".join(game_update.split()).lower()
+    if normalized_game_update in last_toot:
+        return True
+    else:
+        return False   
+
+def post_game_update_toot(game_update):
+    text_to_publish = game_update + " #" + MAST_TAG
+    print("posting this game update:")
+    print(text_to_publish)
+    mastodon.status_post(text_to_publish,None,None,False,"unlisted")
+
+def get_next_game_update():
+    print("Possible toots:")
+    for i, game_update in enumerate(game_updates_list):
+        print(game_update)
+        if is_game_update_in_toot(game_update):
+            return game_updates_list[i-1]
+ 
+
+########################################
+
+# Create webdriver that Selenium uses to access the web page.
+driver = webdriver.Firefox()
+load_page("https://infinitebacklog.net/users/eigrest")
+
+game_updates_elements = filter_game_updates()
+game_updates_list = format_game_updates_text()
 
 driver.close()
 
-# get latest toot from account and tag
+load_dotenv()
+
 mastodon = Mastodon(
     access_token = os.environ['KEY'],
     api_base_url = os.environ['INS']
     )
-last_toot_text = mastodon.account_statuses(mastodon.me().get("id"),False,False,
-                                True,True,mast_tag,
-                                None,None,None,None)[0].content
-print("last toot:")
-print(last_toot_text)
 
-#get to chosen post_text after latest toot posted
-i=0
-chosen_post = ""
-uptodate = False
-print("posible toots:")
-for post_text in posts_texts:
-    print(post_text)
-    if post_text.lower() in last_toot_text.lower():
-        if i<=0:
-            print("Toots up to date")
-            uptodate = True
-        else:
-            chosen_post = posts_texts[i - 1]
-        break
-    else:
-        i=i+1
+last_toot = get_latest_bot_toot()
 
-if not uptodate and chosen_post:
-    # toot post
-    text_to_publish = chosen_post + " #" + mast_tag
-    print("time to toot! " + text_to_publish)
-    mastodon.status_post(text_to_publish,None,None,False,"unlisted")
-elif uptodate:
-    print("no tooting today")
+# if latest update was already posted no toots today. 
+if is_game_update_in_toot(game_updates_list[0]):
+    print("Toots up to date. No tooting today.")
+
+# if no update partially matches with the last toot post oldest update
+elif not any(is_game_update_in_toot(update) for update in game_updates_list):
+    post_game_update_toot(game_updates_list[-1])
+
+# get following update after last toot
 else:
-    print("something wrong happen.")
+    post_game_update_toot(get_next_game_update())
